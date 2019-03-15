089b5b791c639fdd996c15bae707e8a0581917c8
alimehfooz123 <47895314+alimehfooz123@users.noreply.github.com>
2019-03-16 01:18:52 +0300

    Create Emp.java

---
+++ Emp.java
@@ -0,0 +1,37 @@
+package com.tricore.demo;
+
+public class Emp {
+	private int id;
+	private String name;
+	private String status;
+
+	public Empl(int i, String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
